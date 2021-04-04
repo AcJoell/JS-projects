45a5e58c6d551529605d869f0d5ff451df2b0480
AcJoell <joelac04@hotmail.com>
2021-04-03 23:15:37 -0500

    introduccion a valores y variables de JS

---
+++ Basic JS/valores.js
@@ -0,0 +1,28 @@
+// 1. VALORES PRIMITIVOS: Valores básicos
+
+    // 1.1 Valor numerico
+    40
+
+    // Valor de texto (cadena de texto)
+    "Hola"
+
+    //Valor booleano
+    true
+    false
+
+    //Valor vacio
+    null
+    undefined
+
+// 2. VALORES NO PRIMITIVOS O TIPO OBJETO
+
+    // 2.1 Valor Array
+    [1,2,3]
+
+    // 2.2 Valor tipo objeto
+    {nombre: Diego} 
+
+
+//NOTA: Hay una palabra clave llamada typeof que te dice qeu tipo de valor tienes al frente
+//EJ: typeof "" (String) || typeof 40 "number" || typeof Oscar "undefined" || 
+//EJ: typeof true "boolean" || typeof [] "object" || typeof {} "object" ||
---
+++ Basic JS/variables.js
@@ -0,0 +1,26 @@
+// Representacion del lugar donde se van a guardar los valores (; por buena practica)
+var nombre = "Stiven";
+
+// Dos estados e una variable:
+
+    // Declarar
+    var edad;
+
+    // Inicializar
+    edad = 17;
+
+// Array conjunto de elementos
+
+var elementos = ["Computador","Celular","Tablet"];
+
+// Entrar al valor de un array
+
+elementos[1]; //Celular
+
+// Tipo objeto
+
+var persona = {
+    nombre: "Leo",
+    edad: 47
+};
+
